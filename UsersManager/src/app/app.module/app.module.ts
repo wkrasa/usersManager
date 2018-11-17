b599af217d3c1e995f926f662f8381313117f90c
wojciech.krasa <wojciech.krasa@gmail.com>
2018-11-17 13:21:29 +0100

    App componenent moved to separate folder.

--- UsersManager/src/app/app.module/app.module.ts
+++ UsersManager/src/app/app.module/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component/app.component';
 import { CoreModule } from '../core.module/core.module';
 import { SharedModule } from '../shared.module/shared.module';
 
